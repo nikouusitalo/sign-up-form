87d519e2a729890dff69e5c6c8e96fbeb72f0049
Niko <niko.u@hotmail.fi>
2022-04-04 19:59:47 +0300

    Header

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
-
+import Header from  './components/Header/Header.jsx'
 import './App.css';
+import ActionCall from './components/Form/ActionCall/ActionCall.jsx';
 
     function App() {
   return (
     <div className="App">
+        <Header />
+        <section> 
+            <ActionCall />
+        </section>
     </div>
   );
 }
---
+++ app/src/components/Form/ActionCall/ActionCall.jsx
@@ -0,0 +1,10 @@
+const ActionCall = props => {
+    return (
+    <p>
+        Try it free 7 days then $20/mo. thereafter
+        </p>
+
+    );
+}
+
+export default ActionCall
